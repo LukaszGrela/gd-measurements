913b0ae4b49c77575466ded1723b3a4b8870e377
LukaszGrela <lukasz.grela@gmail.com>
2025-08-14 20:13:14 +0200

    linting fixes

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,20 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { Ruler } from "./measurements/ruler/Ruler.tsx";
-import { Grid } from "./measurements/grid/Grid.tsx";
+// import { Grid } from "./measurements/grid/Grid.tsx";
 import { AxisH } from "./measurements/center/AxisH.tsx";
 import { AxisV } from "./measurements/center/AxisV.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    {/* <Ruler />
     <Ruler orientation="horizontal" location="top" />
     <Ruler location="right" />
+    {/*
+     <Ruler />
     <Ruler orientation="horizontal" location="bottom" />
-    <Grid /> */}
+    <Grid /> 
+    */}
     <AxisH />
     <AxisV />
   </StrictMode>
