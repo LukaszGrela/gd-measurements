3fc4661988ee802e4a87f671cdcb3b1333554359
LukaszGrela <lukasz.grela@gmail.com>
2025-08-14 19:23:28 +0200

    configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
@@ -18,5 +19,9 @@ export default defineConfig({
         replacement: resolve(__dirname, "./src/"),
       },
     ],
+  },
+  test: {
+    globals: true, // Enables global test functions like describe, it, etc.
+    environment: "jsdom", // Use jsdom for testing React components
   },
 });
