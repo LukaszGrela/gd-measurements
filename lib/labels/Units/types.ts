fc50da0f167d6857ad8de0db56078c10c932863e
LukaszGrela <lukasz.grela@gmail.com>
2025-11-22 21:29:11 +0100

    Units component to display list of labels for grid and ruler

--- lib/labels/Units/types.ts
+++ lib/labels/Units/types.ts
@@ -1,5 +1,6 @@
 import type { RefObject } from "react";
-import type { TPoint, TSize } from "../types/common";
+import type { TPoint } from "../../types/common";
+import type { IProps as ILabelProps } from "../Label";
 
 export interface IProps {
   orientation: "horizontal" | "vertical";
@@ -11,6 +12,7 @@ export interface IProps {
    * Offset of labels. Defaults to `{x:0, y:0}`
    */
   offset?: number | TPoint;
+
   /**
    * Zero label offset. Defaults to `{ x: 10, y: 0 }`
    */
@@ -18,10 +20,17 @@ export interface IProps {
   /**
    * Size of the label step
    */
-  size: TSize;
+  size: number;
 
   /**
    * Should skip `0` label
    */
   skipZero?: boolean;
+
+  translate?: TPoint;
+
+  debug?: boolean;
+
+  labelConfig?: Pick<ILabelProps, "alignment" | "anchor" | "rotation">;
+  labelZeroConfig?: Pick<ILabelProps, "alignment" | "anchor" | "rotation">;
 }
