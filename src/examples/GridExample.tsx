5c7ad9e49dd5f0213b3ec7786afc1fca6d723341
LukaszGrela <lukasz.grela@gmail.com>
2026-01-14 16:28:30 +0100

    Update grid example to use controls and form data

--- src/examples/GridExample.tsx
+++ src/examples/GridExample.tsx
@@ -1,347 +1,164 @@
-import { useCallback, useMemo, useState, type SVGAttributes } from "react";
+import "./GridExample.scss";
+import { useCallback, useEffect, useState } from "react";
 import { Grid } from "../../lib";
-import type { TPosition } from "../../lib/types/common";
-import type { TUnits } from "../../lib/grid/types";
+import type { TUnits, IProps as IGridProps } from "../../lib/grid/types";
+import { Example } from "./Example";
+import { useFormContext, type FieldValues, type Path } from "react-hook-form";
+import { cloneDeep } from "lodash";
+import { Debug } from "./controls/Debug";
+import { RadioOptions } from "./controls/RadioOptions";
+import { Numeric } from "./controls/Numeric";
+import { Checkbox } from "./controls/Checkbox";
+import { Point } from "./controls/Point";
+import { Size } from "./controls/Size";
+import { LabelPropsControls } from "./controls/LabelPropsControls";
+
+type TGridControlsData = IGridProps & {
+  showLabels: boolean;
+  changeConfig: boolean;
+};
+const defaultValues: TGridControlsData = {
+  debug: false,
+  grid: {
+    height: 50,
+    width: 50,
+    subdivision: 5,
+  },
+  labels: undefined,
+  position: "absolute",
+  changeConfig: false,
+  showLabels: false,
+};
+
+const defaultUnitsValues: TUnits = {
+  hOffset: { x: 2, y: 0 },
+  vOffset: { x: 0, y: -2 },
+  zero: { x: 12, y: 0 },
+  size: { height: 50, width: 50 },
+};
 
 export const GridExample = () => {
-  const [debug, setDebug] = useState(false);
-
-  const [width, setWidth] = useState(50);
-  const [height, setHeight] = useState(50);
-  const [subdivision, setSubdivision] = useState(5);
-  const [position, setPosition] = useState<TPosition>("absolute");
-  const [showLabels, setShowLabels] = useState(false);
-
-  const [changeConfig, setChangeConfig] = useState(false);
-  const [hx, setHXOffset] = useState(0);
-  const [hy, setHYOffset] = useState(0);
-  const [vx, setVXOffset] = useState(0);
-  const [vy, setVYOffset] = useState(0);
-  const [zx, setZXOffset] = useState(10);
-  const [zy, setZYOffset] = useState(0);
-
-  const [unitsWidth, setUnitsWidth] = useState(50);
-  const [unitsHeight, setUnitsHeight] = useState(50);
-
-  const [rotation, setRotation] = useState(0);
-  const [anchor, setAnchor] = useState<"start" | "middle" | "end">("start");
-  const [alignment, setAlignment] = useState<
-    "text-before-edge" | "middle" | "text-after-edge"
-  >("text-before-edge");
-
-  const setDefaults = useCallback(() => {
-    setDebug(false);
-    setWidth(50);
-    setHeight(50);
-    setSubdivision(5);
-    setPosition("absolute");
-    setShowLabels(false);
-    setChangeConfig(false);
-    setHXOffset(0);
-    setHYOffset(0);
-    setVXOffset(0);
-    setVYOffset(0);
-    setZXOffset(10);
-    setZYOffset(0);
-    setUnitsWidth(50);
-    setUnitsHeight(50);
-    setRotation(0);
-    setAnchor("start");
-    setAlignment("text-before-edge");
+  const [controlsData, setControlsData] = useState<IGridProps>(defaultValues);
+  const onControlsDataChanged = useCallback((data: IGridProps) => {
+    setControlsData(data);
   }, []);
 
-  const config = useMemo((): TUnits | boolean => {
-    if (!changeConfig) return showLabels;
-
-    return {
-      hOffset: { x: hx, y: hy },
-      vOffset: { x: vx, y: vy },
-
-      zero: { x: zx, y: zy },
-
-      size: {
-        width: unitsWidth,
-        height: unitsHeight,
-      },
-    };
-  }, [
-    changeConfig,
-    hx,
-    hy,
-    showLabels,
-    unitsHeight,
-    unitsWidth,
-    vx,
-    vy,
-    zx,
-    zy,
-  ]);
-
   return (
-    <div
-      style={{
-        border: "1px solid red",
-        position: "relative",
-        width: "70vw",
-        minHeight: "50vh",
-        margin: "0 auto",
-      }}
-    >
-      <div className="controls">
-        <h2>Grid example</h2>
-        <h3>Controls</h3>
-        <div className="control-group">
-          <button onClick={setDefaults}>Reset</button>
-        </div>
-        <div className="control-group">
-          <label>
-            <input
-              type="checkbox"
-              checked={debug}
-              onChange={(e) => setDebug(e.target.checked)}
-            />
-            Debug
-          </label>
-        </div>
-
-        <div className="control-group">
-          <label>
-            Position:
-            <select
-              value={position}
-              onChange={(e) => setPosition(e.target.value as TPosition)}
-            >
-              {["fixed", "absolute"].map((value, index) => (
-                <option key={`${index}-${value}`} value={value}>
-                  {value}
-                </option>
-              ))}
-            </select>
-          </label>
-        </div>
-        <div className="control-group">
-          <label>
-            Grid size: width
-            <input
-              type="number"
-              min="0"
-              max="150"
-              value={width}
-              onChange={(e) => setWidth(Number(e.target.value))}
-            />
-            , height
-            <input
-              type="number"
-              min="0"
-              max="150"
-              value={height}
-              onChange={(e) => setHeight(Number(e.target.value))}
-            />
-          </label>
-        </div>
-        <div className="control-group">
-          <label>
-            Subdivisions:
-            <input
-              type="number"
-              min="2"
-              max="20"
-              value={subdivision}
-              onChange={(e) => setSubdivision(Number(e.target.value))}
-            />
-            , height
-            <input
-              type="number"
-              min="0"
-              max="50"
-              value={height}
-              onChange={(e) => setHeight(Number(e.target.value))}
-            />
-          </label>
-        </div>
-        <div className="control-group">
-          <label>
-            <input
-              type="checkbox"
-              checked={showLabels}
-              onChange={(e) => setShowLabels(e.target.checked)}
-            />
-            Show labels
-          </label>
-        </div>
-
-        <h4>Label config</h4>
-        <div className="control-group">
-          <label>
-            <input
-              type="checkbox"
-              checked={changeConfig}
-              onChange={(e) => setChangeConfig(e.target.checked)}
-            />
-            Edit config
-          </label>
-        </div>
-
-        {changeConfig && (
-          <div>
-            <div className="control-group">
-              <label>
-                Alignment:
-                <select
-                  value={alignment}
-                  onChange={(e) =>
-                    setAlignment(
-                      e.target.value as
-                        | "text-before-edge"
-                        | "middle"
-                        | "text-after-edge"
-                    )
-                  }
-                >
-                  {(
-                    [
-                      "text-before-edge",
-                      "middle",
-                      "text-after-edge",
-                    ] as Exclude<
-                      SVGAttributes<SVGTSpanElement>["alignmentBaseline"],
-                      undefined
-                    >[]
-                  ).map((value, index) => (
-                    <option key={`${index}-${value}`} value={value}>
-                      {value}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-
-            <div className="control-group">
-              <label>
-                Anchor:
-                <select
-                  value={anchor}
-                  onChange={(e) =>
-                    setAnchor(e.target.value as "start" | "middle" | "end")
-                  }
-                >
-                  {["start", "middle", "end"].map((value, index) => (
-                    <option key={`${index}-${value}`} value={value}>
-                      {value}
-                    </option>
-                  ))}
-                </select>
-              </label>
-            </div>
-
-            <div className="control-group">
-              <label>
-                Rotation: {rotation}
-                <input
-                  type="range"
-                  min="-180"
-                  max="180"
-                  step="45"
-                  value={rotation}
-                  onChange={(e) => setRotation(Number(e.target.value))}
-                />
-              </label>
-            </div>
-
-            <div className="control-group">
-              <label>
-                Units size: width
-                <input
-                  type="number"
-                  min="0"
-                  max="150"
-                  value={unitsWidth}
-                  onChange={(e) => setUnitsWidth(Number(e.target.value))}
-                />
-                , height
-                <input
-                  type="number"
-                  min="0"
-                  max="150"
-                  value={unitsHeight}
-                  onChange={(e) => setUnitsHeight(Number(e.target.value))}
-                />
-              </label>
-            </div>
-            <div className="control-group">
-              <label>
-                Horizontal label offset: x
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={hx}
-                  onChange={(e) => setHXOffset(Number(e.target.value))}
-                />
-                , y
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={hy}
-                  onChange={(e) => setHYOffset(Number(e.target.value))}
-                />
-              </label>
-            </div>
-            <div className="control-group">
-              <label>
-                Vertical label offset: x
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={vx}
-                  onChange={(e) => setVXOffset(Number(e.target.value))}
-                />
-                , y
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={vy}
-                  onChange={(e) => setVYOffset(Number(e.target.value))}
-                />
-              </label>
-            </div>
-            <div className="control-group">
-              <label>
-                Zero label offset: x
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={zx}
-                  onChange={(e) => setZXOffset(Number(e.target.value))}
-                />
-                , y
-                <input
-                  type="number"
-                  min="0"
-                  max="50"
-                  value={zy}
-                  onChange={(e) => setZYOffset(Number(e.target.value))}
-                />
-              </label>
-            </div>
-          </div>
-        )}
-      </div>
-
-      <Grid
-        position={position}
-        labels={config}
-        debug={debug}
-        grid={{
-          width,
-          height,
-          subdivision,
-        }}
-      />
-    </div>
+    <>
+      <Example<TGridControlsData>
+        defaultValues={defaultValues}
+        className="GridExample"
+        title="Grid example"
+        controls={<GridControls onChange={onControlsDataChanged} />}
+      >
+        <Grid {...controlsData} />
+      </Example>
+    </>
   );
 };
+function GridControls({ onChange }: { onChange: (data: IGridProps) => void }) {
+  const methods = useFormContext<TGridControlsData>();
+
+  useEffect(() => {
+    // make sure to unsubscribe;
+    const callback = methods.subscribe({
+      formState: {
+        values: true,
+      },
+      callback: ({ values, name }) => {
+        const { showLabels, changeConfig, ...props } = values;
+        if (name === "showLabels") {
+          if (!showLabels) {
+            props.labels = undefined;
+            methods.setValue("labels", undefined);
+            methods.setValue("changeConfig", false);
+          } else {
+            props.labels = true;
+            methods.setValue("labels", true);
+          }
+        }
+        if (name === "changeConfig") {
+          if (changeConfig) {
+            // apply defaults
+            props.labels = cloneDeep(defaultUnitsValues);
+            methods.setValue("labels", cloneDeep(defaultUnitsValues));
+          } else {
+            props.labels = true;
+            methods.setValue("labels", true);
+          }
+        }
+
+        onChange?.(props);
+      },
+    });
+
+    return () => callback();
+
+    // You can also just return the subscribe
+    // return subscribe();
+  }, [methods, onChange]);
+
+  return (
+    <>
+      <Debug />
+      <RadioOptions<TGridControlsData>
+        label="Position"
+        name="position"
+        options={[{ value: "absolute" }, { value: "fixed" }]}
+      />
+      {/* grid subdivision */}
+      <Numeric<TGridControlsData>
+        name="grid.subdivision"
+        label="Subdivision of the Grid box"
+      />
+      {/* size */}
+      <Size<TGridControlsData> name="grid" label="Grid" />
+
+      <Checkbox<TGridControlsData> label="Show labels" name="showLabels" />
+      {methods.getValues("showLabels") && (
+        <>
+          <div className="control-group">
+            <h4>Labels config</h4>
+            <Checkbox<TGridControlsData>
+              label="Edit config"
+              name="changeConfig"
+            />
+            {methods.getValues("changeConfig") && (
+              <UnitsPropsControls<TGridControlsData> name="labels" />
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+}
+
+function UnitsPropsControls<FormData extends FieldValues>({
+  name,
+}: {
+  name: Path<FormData>;
+}) {
+  return (
+    <>
+      {/* TUnit */}
+      <Point name={`${name}.hOffset`} label="Offset of horizontal labels" />
+      <Point name={`${name}.vOffset`} label="Offset of vertical labels" />
+      <Point name={`${name}.zero`} label="Offset of zero label" />
+      <Size name={`${name}.size`} label="Size of the label step" />
+      <div className="flex-row">
+        <LabelPropsControls
+          label="Vertical label config"
+          name={`${name}.vLabelConfig`}
+        />
+        <LabelPropsControls
+          label="Horizontal label config"
+          name={`${name}.hLabelConfig`}
+        />
+      </div>
+      <LabelPropsControls
+        label="labelZeroConfig"
+        name={`${name}.labelZeroConfig`}
+      />
+    </>
+  );
+}
