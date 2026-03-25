776fb591f37ce672d448324f087d6c1eb78cb4a7
LukaszGrela <lukasz.grela@gmail.com>
2026-03-25 14:01:57 +0100

    version bump

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 // import dts from "unplugin-dts/vite";
 import dts from "vite-plugin-dts";
@@ -22,46 +22,54 @@ function emptyDir(dir: string) {
 emptyDir(resolve(__dirname, "dist"));
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    dts({
-      include: ["lib"],
-      exclude: ["src", "**/*.test.*"],
-      tsconfigPath: "./tsconfig.lib.json",
-    }),
-  ],
-
-  build: {
-    copyPublicDir: false,
-    lib: {
-      entry: resolve(currentDir, "lib/index.ts"),
-      formats: ["es"],
-    },
-    rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
-    },
-  },
-
-  resolve: {
-    alias: [
-      {
-        // SASS TILDE ALIAS
-        find: /^~(.*)$/,
-        replacement: "$1",
-      },
-      {
-        find: /^@\/lib\//,
-        replacement: resolve(currentDir, "./lib/"),
-      },
-      {
-        find: /^@\/(?!lib\/)/,
-        replacement: resolve(currentDir, "./src/"),
-      },
+export default defineConfig(
+  (): UserConfig => ({
+    plugins: [
+      react(),
+      dts({
+        include: ["lib"],
+        exclude: ["src", "**/*.test.*"],
+        tsconfigPath: "./tsconfig.lib.json",
+      }),
     ],
-  },
-  test: {
-    globals: true, // Enables global test functions like describe, it, etc.
-    environment: "jsdom", // Use jsdom for testing React components
-  },
-});
+
+    build: {
+      copyPublicDir: false,
+      lib: {
+        entry: resolve(currentDir, "lib/index.ts"),
+        formats: ["es"],
+      },
+      rollupOptions: {
+        external: [
+          "react",
+          "react-dom",
+          "react-dom/client",
+          "react/jsx-runtime",
+        ],
+      },
+      minify: false,
+    },
+
+    resolve: {
+      alias: [
+        {
+          // SASS TILDE ALIAS
+          find: /^~(.*)$/,
+          replacement: "$1",
+        },
+        {
+          find: /^@\/lib\//,
+          replacement: resolve(currentDir, "./lib/"),
+        },
+        {
+          find: /^@\/(?!lib\/)/,
+          replacement: resolve(currentDir, "./src/"),
+        },
+      ],
+    },
+    test: {
+      globals: true, // Enables global test functions like describe, it, etc.
+      environment: "jsdom", // Use jsdom for testing React components
+    },
+  }),
+);
